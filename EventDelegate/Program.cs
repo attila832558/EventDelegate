1278542c142581ba862d8ca6bd7b7205fd89324a
attila832558 <ubikutya2003@gmail.com>
2022-12-09 11:26:41 +0100

    Initial commit

---
+++ EventDelegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
